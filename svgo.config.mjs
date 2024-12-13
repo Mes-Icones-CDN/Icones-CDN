9db8c045ffefc188f9ccb37858addf72f59797cf
DjoAHP <djodjo.vely@gmail.com>
2024-12-14 00:12:17 +0100

    CADRE AJOUT A ICONES ROOT

---
+++ svgo.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  plugins: [
+    {
+      name: "removeAttrs",
+      params: { attrs: "(fill|stroke)" },
+    },
+    {
+      name: "addAttributesToSVGElement",
+      params: {
+        attributes: [
+          {
+            fill: "currentColor",
+          },
+        ],
+      },
+    },
+  ],
+};
